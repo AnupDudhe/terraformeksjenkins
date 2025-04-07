a186df45a3ea45977d1557cb0a0069f8bc335c75
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-04-07 14:56:43 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,54 +1,59 @@
-pipeline {
-  agent any
+provider "aws" {
+  region = "ap-south-1"
+  profile = configs
+}
 
-  environment {
-    AWS_REGION = "ap-south-1"
-    TF_IN_AUTOMATION = "true"
-  }
+variable "cluster_name" {
+  default = "my-eks-cluster"
+}
 
-  tools {
-    terraform 'terraform-latest' // Or whatever name you configured in Jenkins tools
-  }
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.1"
 
-  stages {
-    stage('Checkout Code') {
-      steps {
-        git branch: 'main', url: 'https://github.com/your-user/terraform-eks.git'
-      }
-    }
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
 
-    stage('Terraform Init') {
-      steps {
-        withCredentials([[
-          $class: 'AmazonWebServicesCredentialsBinding',
-          credentialsId: 'aws-creds'
-        ]]) {
-          sh 'terraform init'
-        }
-      }
-    }
+  azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-    stage('Terraform Plan') {
-      steps {
-        withCredentials([[
-          $class: 'AmazonWebServicesCredentialsBinding',
-          credentialsId: 'aws-creds'
-        ]]) {
-          sh 'terraform plan -out=tfplan'
-        }
-      }
-    }
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
 
-    stage('Terraform Apply') {
-      steps {
-        input message: "Do you want to apply these changes?"
-        withCredentials([[
-          $class: 'AmazonWebServicesCredentialsBinding',
-          credentialsId: 'aws-creds'
-        ]]) {
-          sh 'terraform apply -auto-approve tfplan'
-        }
-      }
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.3"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    default_node_group = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+
+      instance_types = ["t3.medium"]
     }
   }
+}
+
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  value = module.eks.cluster_security_group_id
+}
+
+output "node_group_role_arn" {
+  value = module.eks.eks_managed_node_groups["default_node_group"].iam_role_arn
 }
